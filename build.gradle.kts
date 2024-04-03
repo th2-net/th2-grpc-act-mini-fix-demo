ed352c176f59fd5707011cee9831a1a208c8a8e2
Oleg Smelov <oleg.smelov@exactpro.com>
2024-04-03 23:33:13 +0400

    build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,41 @@
+plugins {
+    id("java-library")
+    id("maven-publish")
+
+    val th2PluginVersion = "0.0.4"
+    id("com.exactpro.th2.gradle.base") version th2PluginVersion
+    id("com.exactpro.th2.gradle.grpc") version th2PluginVersion
+    id("com.exactpro.th2.gradle.publish") version th2PluginVersion
+}
+
+allprojects {
+    val version = project.properties["release_version"].toString()
+    val suffix = project.properties["version_suffix"]?.toString() ?: ""
+    this.group = "com.exactpro.th2"
+    this.version = version + if (suffix.isEmpty()) "" else "-$suffix"
+}
+
+configurations.all {
+    resolutionStrategy.cacheChangingModulesFor(0, "seconds")
+    resolutionStrategy.cacheDynamicVersionsFor(0, "seconds")
+}
+
+th2Grpc {
+    service.set(true)
+}
+
+dependencies {
+    implementation("com.exactpro.th2:grpc-hand:3.0.0-RM-84612-8544324408-SNAPSHOT")
+}
+
+repositories {
+    mavenCentral()
+    maven {
+        name = "Sonatype_snapshots"
+        url = uri("https://s01.oss.sonatype.org/content/repositories/snapshots/")
+    }
+    maven {
+        name = "Sonatype_releases"
+        url = uri("https://s01.oss.sonatype.org/content/repositories/releases/")
+    }
+}
